c9f3c673e92b0fd2f018f27cf27528af843b89ed
noblelar <59696875+noblelar@users.noreply.github.com>
2024-05-18 09:14:06 +0100

    BubbleSort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,29 @@
 export default function bubble_sort(arr: number[]): void {
+    // ! My personal Version of the Sorting algorithm
+    let itemsNum = arr.length;
+    let i = itemsNum;
 
+    while (i > 1) {
+        arr.forEach((item, it) => {
+            const nextitem = arr[it + 1];
+            // const currentItem = item;
+
+            if (item > nextitem) {
+                arr[it + 1] = item;
+                arr[it] = nextitem;
+            }
+        });
+        i--;
+    }
+
+   //  ! Sample from ...
+   //  for (let i = 0; i < arr.length; ++i) {
+   //      for (let j = 0; j < arr.length - 1 - i; ++j) {
+   //          if (arr[j] > arr[j + 1]) {
+   //              const tmp = arr[j];
+   //              arr[j] = arr[j + 1];
+   //              arr[j + 1] = tmp;
+   //          }
+   //      }
+   //  }
 }
