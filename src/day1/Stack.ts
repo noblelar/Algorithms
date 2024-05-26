477d44b04771a6f7055a4d1e7e2fa1c7ec9688fd
noblelar <59696875+noblelar@users.noreply.github.com>
2024-05-26 23:56:07 +0100

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -8,9 +8,19 @@ export default class Stack<T> {
     private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const node = {value: item} as Node<T>;
+        this.length++;
+        if(!this.head){
+            this.head = node;
+            return
+        }
+
+        
 
 }
     pop(): T | undefined {
