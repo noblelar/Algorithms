bc0096c405c5f78f0f1e4b39cffbba4eddd95056
noblelar <59696875+noblelar@users.noreply.github.com>
2024-05-27 04:03:19 +0100

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,7 +1,7 @@
-type Node<T> ={
-    value: T,
-    prev?:Node<T>
-}
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
 
 export default class Stack<T> {
     public length: number;
@@ -13,20 +13,46 @@ export default class Stack<T> {
     }
 
     push(item: T): void {
-        const node = {value: item} as Node<T>;
+        const node = { value: item } as Node<T>;
         this.length++;
-        if(!this.head){
+        if (!this.head) {
             this.head = node;
-            return
+            return;
         }
 
-        
-
-}
+        node.prev = this.head;
+        this.head = node;
+        // this.head.prev = node;
+    }
     pop(): T | undefined {
 
-}
-    peek(): T | undefined {
+        this.length = Math.max(0, this.length-1);
+        if(this.length === 0){
+            const head = this.head;
+            this.head = undefined;
+            return head?.value
+        }
 
-}
+        const head = this.head as Node<T>;
+        this.head = head.prev;
+        return head.value;
+
+        // ! My Version 
+        // if (!this.head) {
+        //     return undefined;
+        // }
+        // this.length--;
+
+        // const head = this.head;
+        // this.head = this.head?.prev;
+
+        // head.prev = undefined;
+
+        // if (this.length === 0) this.head = undefined;
+
+        // return head.value;
+    }
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
